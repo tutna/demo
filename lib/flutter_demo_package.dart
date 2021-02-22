fb5acc4bf9b376cb2a46480ddc2ab33cd5ecfb57
AnhTu <tutna@antsprogrammatic.com>
2021-02-22 14:28:57 +0700

    first commit

---
+++ lib/flutter_demo_package.dart
@@ -0,0 +1,9 @@
+library flutter_demo_package;
+//
+// /// A Calculator.
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
+
+export 'src/splash.dart';
